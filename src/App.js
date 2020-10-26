ad323502120fd0897a03a9458c9589ffe19c7cad
Austin Bell <xavierbellhaddon@gmail.com>
2020-10-25 20:45:08 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles.js";
 
 const alanKey = "7ed7aa91b012589ff8da048870e0c5032e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
+  const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
@@ -19,7 +21,10 @@ const App = () => {
     }, [])
   return (
     <div>
-      <h1>Alan AI News Application</h1>
+      <div className={classes.logoContainer}>
+        <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo" />
+
+      </div>
       <NewsCards articles={newsArticles} />
     </div>
   );
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -8,22 +8,25 @@ import {
   Button,
   Typography,
 } from "@material-ui/core";
+import useStyles from "./styles.js"
 
 const NewsCard = ({
   article: { description, title, publishedAt, source, url, urlToImage },
   i,
 }) => {
+    const classes = useStyles();
   return (
     <div>
-      <Card>
-        <CardActionArea>
+      <Card className={classes.card}>
+        <CardActionArea href={url} target="_blank">
           <CardMedia
+             className={classes.media}
             image={
               urlToImage ||
               "https://images.pexels.com/photos/4057663/pexels-photo-4057663.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
             }
           />
-          <div>
+          <div className={classes.details}>
             <Typography
               variant="body2"
               color="textSecondary"
@@ -35,7 +38,7 @@ const NewsCard = ({
               component="h2"
             >{source.name}</Typography>
           </div>
-          <Typography gutterBottom varian="h5">{title}</Typography>
+          <Typography gutterBottom varian="h5" className={classes.title}>{title}</Typography>
           <CardContent>
             <Typography
               variant="body2"
@@ -44,7 +47,7 @@ const NewsCard = ({
             >{description}</Typography>
           </CardContent>
         </CardActionArea>
-        <CardActions>
+        <CardActions className={classes.cardActions}>
           <Button size="small" color="primary">Learn More</Button>
           <Typography variant="h5" color="textSecondary">{i + 1}</Typography>
         </CardActions>
